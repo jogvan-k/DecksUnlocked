71b2af352950a399e88950ae2825737363be51c6
Jógvan Kjølbro <jogvan_k@hotmail.com>
2020-09-17 18:30:08 +0200

    Improved ai logging and added benchmark tests

--- src/KeyforgeUnlocked.Test/Benchmark/BenchmarkTest.cs
+++ src/KeyforgeUnlocked.Test/Benchmark/BenchmarkTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Timers;
 using KeyforgeUnlocked.States;
 using KeyforgeUnlocked.Types;
 using KeyforgeUnlockedConsole.ConsoleGames;
@@ -13,7 +14,8 @@ namespace KeyforgeUnlockedTest.Benchmark
   [TestFixture]
   sealed class BenchmarkTest
   {
-    ICoreState _state;
+    IState _state;
+
     [Test]
     [Explicit]
     public void StateRun()
@@ -22,10 +24,15 @@ namespace KeyforgeUnlockedTest.Benchmark
       _state = SetupStartState();
 
       var stopwatch = Stopwatch.StartNew();
-      while (!((IState)_state).IsGameOver && ++i < 50000)
+      while (!_state.IsGameOver && ++i > -1)
       {
-        _state = _state.Actions().First().DoCoreAction();
+        var coreActions = _state.Actions();
+        if (coreActions.Length > 1)
+          _state = (IState) coreActions[1].DoCoreAction();
+        else
+          _state = (IState) coreActions.First().DoCoreAction();
       }
+
       stopwatch.Stop();
       Console.WriteLine($"Ran through {i} states in {stopwatch.Elapsed.TotalSeconds} seconds.");
     }
@@ -35,12 +42,40 @@ namespace KeyforgeUnlockedTest.Benchmark
     public void MinimaxAIRun()
     {
       _state = SetupStartState();
-      var ai = new MinimaxAI(new Evaluator(), 3, SearchDepthConfiguration.turn, AIMethods.LoggingConfiguration.LogAll);
+      var ai = new MinimaxAI(new Evaluator(), 2, SearchDepthConfiguration.turn, AIMethods.LoggingConfiguration.LogAll);
 
       ((IGameAI) ai).DetermineAction(_state);
-      
-      Console.WriteLine($"Evaluated {ai.logInfo.nodesEvaluated} end states in {ai.logInfo.elapsedTime.TotalSeconds} seconds.");
-      Console.WriteLine($"{ai.logInfo.successfulHashMapLookups} successful hash map lookups and {ai.logInfo.prunedPaths} paths pruned.");
+
+      Console.WriteLine(
+        $"Evaluated {ai.LatestLogInfo.nodesEvaluated} end states in {ai.LatestLogInfo.elapsedTime.TotalSeconds} seconds.");
+      Console.WriteLine(
+        $"{ai.LatestLogInfo.successfulHashMapLookups} successful hash map lookups and {ai.LatestLogInfo.prunedPaths} paths pruned.");
+    }
+
+    [Test]
+    [Explicit]
+    public void FullGameRun()
+    {
+      _state = SetupStartState();
+      var ai = new MinimaxAI(new Evaluator(), 4, SearchDepthConfiguration.actions, AIMethods.LoggingConfiguration.LogAll);
+
+      var playerTurn = _state.PlayerTurn;
+      while (!_state.IsGameOver)
+      {
+        var moves = ((IGameAI) ai).DetermineAction(_state);
+        while (!_state.IsGameOver && _state.PlayerTurn == playerTurn && moves.Length > 0)
+        {
+          _state = (IState) _state.Actions()[moves[0]].DoCoreAction();
+          moves = moves.Skip(1).ToArray();
+        }
+
+        playerTurn = playerTurn.Other();
+      }
+
+      Console.WriteLine(
+        $"Evaluated {ai.logInfos.Sum(l => l.nodesEvaluated)} end states over {ai.logInfos.Length} calls in {ai.logInfos.Sum(l => l.elapsedTime.TotalSeconds)} seconds.");
+      Console.WriteLine(
+        $"{ai.logInfos.Sum(l => l.successfulHashMapLookups)} successful hash map lookups and {ai.logInfos.Sum(l => l.prunedPaths)} paths pruned.");
     }
 
     ImmutableState SetupStartState()
--- src/UnlockedCore/AITypes.fs
+++ src/UnlockedCore/AITypes.fs
@@ -5,53 +5,60 @@ open UnlockedCore
 open AIMethods
 
 type SearchDepthConfiguration =
-    | node = 0
+    | actions = 0
     | turn = 1
 
 module UtilMethods =
     let toSearchLimit searchConfiguration depth tn =
                         match searchConfiguration with
-                        | SearchDepthConfiguration.node -> Depth depth
+                        | SearchDepthConfiguration.actions -> Depth depth
                         | SearchDepthConfiguration.turn -> Until (tn + depth, 0)
                         | _ ->  raise (ArgumentException("Undefined SearchDepthConfiguration"))
 
-[<Struct>]
 type LogInfo =
-    { mutable nodesEvaluated: int
-      mutable elapsedTime: TimeSpan
-      mutable prunedPaths: int
-      mutable successfulHashMapLookups: int}
+    struct
+        val mutable nodesEvaluated: int
+        val mutable elapsedTime: TimeSpan
+        val mutable prunedPaths: int
+        val mutable successfulHashMapLookups: int
+    end
 
 type MinimaxAI(evaluator : IEvaluator, depth, searchConfiguration: SearchDepthConfiguration, loggingConfiguration : LoggingConfiguration) =
-    
-    [<DefaultValue>] val mutable logInfo: LogInfo
     let logEvaulatedStates = loggingConfiguration.HasFlag(LoggingConfiguration.LogEvaluatedStates)
     let logTime = loggingConfiguration.HasFlag(LoggingConfiguration.LogTime)
-    let timer = if(logTime)
-                then Some(System.Diagnostics.Stopwatch.StartNew())
-                else None
-
+    let mutable _logInfos: LogInfo list = List.empty
+    member this.logInfos
+        with get () = _logInfos
+        
+    member this.LatestLogInfo = this.logInfos.Head
+    
     interface IGameAI with
         member this.DetermineAction s =
+            let mutable logInfo = LogInfo()
+            let timer = if(logTime)
+                        then Some(System.Diagnostics.Stopwatch.StartNew())
+                        else None
+            
             let d = UtilMethods.toSearchLimit searchConfiguration depth s.TurnNumber
             
             let evaluate =
                 if(logEvaulatedStates)
                 then
-                    function i-> this.logInfo.nodesEvaluated <- this.logInfo.nodesEvaluated + 1
+                    function i-> logInfo.nodesEvaluated <- logInfo.nodesEvaluated + 1
                                  evaluator.Evaluate i
                 else evaluator.Evaluate
             
             let accumulator = accumulator(evaluate, loggingConfiguration)
             let returnVal = minimaxAI accumulator d s |> snd |> List.toArray
 
-            this.logInfo.elapsedTime <- if(timer.IsSome)
+            logInfo.elapsedTime <- if(timer.IsSome)
                                         then timer.Value.Stop()
                                              timer.Value.Elapsed
                                         else TimeSpan()
-            this.logInfo.prunedPaths <- accumulator.prunedPaths
-            this.logInfo.successfulHashMapLookups <- accumulator.successfulHashMapLookups
+            logInfo.prunedPaths <- accumulator.prunedPaths
+            logInfo.successfulHashMapLookups <- accumulator.successfulHashMapLookups
             
+            _logInfos <- logInfo :: _logInfos
             returnVal
 
 type RandomMoveAI() =
@@ -59,4 +66,4 @@ type RandomMoveAI() =
 
     interface IGameAI with
         member this.DetermineAction s =
-            AIMethods.randomMoveAI rng s |> Array.singleton
+            randomMoveAI rng s |> Array.singleton

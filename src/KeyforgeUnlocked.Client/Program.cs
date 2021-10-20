75b7ffd63fba70377a3d2f42395fe978508c039b
Jógvan Kjølbro <jogvan.kjolbro@nexta.io>
2021-10-20 18:00:15 +0200

    Added gRPC server/client projects.

---
+++ src/KeyforgeUnlocked.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using KeyforgeUnlocked.Server;
+
+namespace KeyforgeUnlocked.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions{HttpClient = new HttpClient(new HttpClientHandler{ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator})});
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
